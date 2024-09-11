6cf2e229ebb775c8d300ceb2215c01bb4be83678
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-11 00:08:06 -0400

    env implements

--- src/APIKEY_SECRETS/FIREBASE_KEY.js
+++ src/APIKEY_SECRETS/FIREBASE_KEY.js
@@ -11,13 +11,13 @@
 // export default FIREBASE_KEY;
 
 const FIREBASE_KEY = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    apiKey: import.meta.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: import.meta.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: import.meta.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 }
 
 export default FIREBASE_KEY;
