86be1945f3b1b9ee1cb840610dcc0d6a6332dfa0
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-11 23:16:54 -0400

    Installed tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,6 @@ function App() {
     return (
         <>
             <div >
-
-                <h1 className="text-5xl font-bold text-sky-600" >
-                    Tailwind Test
-                </h1 >
-
                 <a href="https://vitejs.dev" target="_blank" >
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a >
