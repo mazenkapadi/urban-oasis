61f853ba2a2c0db9d22568c45f79cdf13c220b04
rosstopher14 <144082134+rosstopher14@users.noreply.github.com>
2024-09-14 22:02:26 -0400

    Event Creation 9-16

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,5 +1,3 @@
-import firebase from "firebase/compat/app";
-import "firebase/firestore";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
