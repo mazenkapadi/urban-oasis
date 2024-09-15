42503910c91647b3217806aa458711a5505dfd79
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-15 18:23:56 -0400

    Event Card 9-16

---
+++ src/pages/EventCardPage.jsx
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import EventCard from "../components/EventCard.jsx";
+
+function EventCardPage() {
+
+    return (
+        <>
+            <div className="flex items-center w-screen">
+                <EventCard />
+
+            </div>
+        </>
+    );
+}
+
+export default EventCardPage;
