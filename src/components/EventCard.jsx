80b3f1cee09002dc0baa447dc49c488984dd71f6
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-15 18:23:56 -0400

    Event Card 9-17

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -7,10 +7,11 @@ function EventCard() {
 
     return (
         <>
-            <div className="event-card h-3/5 p-5">
-                <div className="box-border rounded-lg bg-gray-900 p-3 flex w-full bg-cover bg-center hover:scale-110"
-                     style={{backgroundImage: "url('src/assets/backgroundTestImage.jpg')"}}>
-                    <div className="flex flex-col p-4">
+            <div className="event-card p-3">
+                <div className="box-border rounded-lg bg-gray-900 p-3 flex"
+                     style={{backgroundImage: "url('your-image.jpg')"}}>
+                    <div className="flex flex-col p-6">
+                        <label className="block text-gray-200 opacity-50 pb-1">Date</label>
                         <label className="block text-gray-300 pb-1 text-2xl">Event Title</label>
                         <div className={"flex flex-row"}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
@@ -23,15 +24,21 @@ function EventCard() {
                             <label className="block text-gray-200 opacity-50 pb-1">Event Location</label>
                         </div>
 
-                        <div className="flex flex-row pt-6 gap-32">
-                            <label className="block text-gray-200 opacity-50 pb-1">Date</label>
+                        <div className="flex flex-row pt-20 gap-32">
+                            <label className="block text-gray-300 opacity-50 pb-1">Capacity</label>
                             <label className="block text-gray-300 opacity-50 pb-1">Ticket Price</label>
                         </div>
 
                         <div className="flex flex-row gap-32">
-                            <label className="block text-gray-300 pb-1 text-2xl">Date</label>
+                            <label className="block text-gray-300 pb-1 text-2xl">{capacity}/{totalCapacity}</label>
                             <label className="block text-gray-300 pb-1 text-2xl">${ticketPrice.toFixed(2)}</label>
                         </div>
+                        <button
+                            className="bg-white text-black font-bold py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline w-full mt-4"
+                            aria-label="View Event"
+                        >
+                            View Event
+                        </button>
                     </div>
                 </div>
             </div>
