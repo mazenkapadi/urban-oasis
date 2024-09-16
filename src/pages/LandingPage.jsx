081596cbebae7e83e8ea4fdc59917e4e1260fd83
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-16 18:10:08 -0400

    Final

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 function LandingPage() {
+
+    const navigate = useNavigate();
+
+    const handleSignIn = () => {
+        navigate('/signIn')
+    }
 
     return (
         <>
-            <div>
-                Hey welcome to the landing page
-            </div>
+            {/*<HeaderComponent />*/}
+            <div className="p-8" >
+                <h3 className="text-5xl text-slate-700">Hey welcome to the landing page</h3 >
+                <div className="pt-8">
+                    <hr className=""></hr >
+                    <button className="box-border border-4 rounded-lg bg-gray-900 text-gray-200 px-6 py-2 flex items-center justify-center"
+                            onClick={handleSignIn}
+                    > Sign In
+                    </button >
+                </div >
+            </div >
+            {/*<FooterComponent />*/}
         </>
     )
 }
