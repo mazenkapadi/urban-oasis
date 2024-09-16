6d2f3174eb2ca4b5b07ef443df34d602f712d457
mafareed <azmat.fareed123@gmail.com>
2024-09-17 00:54:28 +0500

    Update structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,39 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-    createBrowserRouter,
-    RouterProvider,
-} from "react-router-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import './index.css'
+import "./index.css";
 import SignInPage from "./pages/SignInPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
-import NotFoundPage from "./pages/404NotFound.jsx";
 import NotFound from "./pages/404NotFound.jsx";
 
-
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <LandingPage />
-    },
-    {
-        path: "/signIn",
-        element: <SignInPage />
-    },
-    {
-        path: "/signUp",
-        element: <SignUpPage />
-    },
-    {
-        path: "/forgotPassword",
-        element: <ForgotPasswordPage />
-    },
-    {
-        path: "*",
-        element: <NotFound />
-    },
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/signIn",
+    element: <SignInPage />,
+  },
+  {
+    path: "/signUp",
+    element: <SignUpPage />,
+  },
+  {
+    path: "/forgotPassword",
+    element: <ForgotPasswordPage />,
+  },
+  {
+    path: "*",
+    element: <NotFound />,
+  },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
-    <StrictMode >
-        <RouterProvider router={router} />
-    </StrictMode >
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
