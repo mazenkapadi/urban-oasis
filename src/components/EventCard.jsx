9b9bff156c2dcc2388fbbf1615679cb8ece4ee15
rosstopher14 <144082134+rosstopher14@users.noreply.github.com>
2024-09-17 13:41:59 -0400

    Event Card 9-17

--- src/components/EventCard.jsx
+++ src/components/EventCard.jsx
@@ -12,7 +12,16 @@ function EventCard() {
                      style={{backgroundImage: "url('src/assets/backgroundTestImage.jpg')"}}>
                     <div className="flex flex-col p-4">
                         <label className="block text-gray-300 pb-1 text-2xl">Event Title</label>
-                        <label className="block text-gray-200 opacity-50 pb-1">Event Location</label>
+                        <div className={"flex flex-row"}>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
+                                 stroke="white" className="size-6" style={{ opacity: .5}}>
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"/>
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"/>
+                            </svg>
+                            <label className="block text-gray-200 opacity-50 pb-1">Event Location</label>
+                        </div>
 
                         <div className="flex flex-row pt-6 gap-32">
                             <label className="block text-gray-200 opacity-50 pb-1">Date</label>
