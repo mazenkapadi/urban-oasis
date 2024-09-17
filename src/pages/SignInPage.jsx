103d9cb048dfba2ec49909ea44a6d63940151f2a
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-16 21:50:54 -0400

    Final

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import signIn from "../services/auth/signIn.js";
 import AuthLeftComponent from "../components/AuthLeftComponent.jsx";
 
@@ -7,7 +7,7 @@ function SignInPage() {
     const [ password, setPassword ] = useState('');
     const [ error, setError ] = useState(null);
 
-    const EMAIL_REGEX = /^[A-Za-z0-9+_.-]+@(.+)$/;
+    const EMAIL_REGEX = /^[A-Za-z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
     const validateEmail = (email) => EMAIL_REGEX.test(email);
 
