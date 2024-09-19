1b015d9f089528f08a87a79a16f58178e1310c93
mafareed <azmat.fareed123@gmail.com>
2024-09-20 02:01:05 +0500

    add: current weather api

--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,16 @@ import SignUpPage from "./pages/SignUpPage.jsx";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import NotFound from "./pages/404NotFound.jsx";
+
 import UserProfilePage from "./pages/UserProfilePage.jsx";
 import ContactInfoPage from './pages/ContactInfoPage.jsx';
 import SettingsPage from './pages/SettingsPage.jsx';
 import PaymentsPage from './pages/PaymentsPage.jsx';
 import SupportPage from './pages/SupportPage.jsx';
 import UserProfileContent from './components/UserProfileContent.jsx';
+
+import UserPage from "./pages/UserProfile.jsx";
+import WeatherPage from "./pages/WeatherPage.jsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import PhotoCarousel from "./components/PhotoCarousel.jsx";
@@ -56,6 +60,10 @@ const router = createBrowserRouter([
     {
         path: "*",
         element: <NotFound />
+    },
+    {
+      path: "/weather",
+      element: <WeatherPage />,
     },
 ]);
 
