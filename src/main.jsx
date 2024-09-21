6fc5b4d53ba9b5d3eea2bec7253b9a5a1d7578a9
rosstopher14 <144082134+rosstopher14@users.noreply.github.com>
2024-09-21 19:23:10 -0400

    Event Page  9-21

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-import "./index.css";
+import './index.css';
 import SignInPage from "./pages/SignInPage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage.jsx";
 import LandingPage from "./pages/LandingPage.jsx";
 import NotFound from "./pages/404NotFound.jsx";
-import UserPage from "./pages/UserProfilePage.jsx";
-import EventPage from "./pages/EventPage.jsx";
+import UserProfilePage from "./pages/UserProfilePage.jsx";
+import ContactInfoPage from './pages/ContactInfoPage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
+import PaymentsPage from './pages/PaymentsPage.jsx';
+import SupportPage from './pages/SupportPage.jsx';
+import UserProfileContent from './components/UserProfileContent.jsx';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import PhotoCarousel from "./components/PhotoCarousel.jsx";
 
 const router = createBrowserRouter([
     {
@@ -29,12 +39,19 @@ const router = createBrowserRouter([
         element: <ForgotPasswordPage />
     },
     {
-        path: "/userPage",
-        element: <UserPage />
+        path: "/userProfilePage",
+        element: <UserProfilePage />,
+        children: [
+            { index: true, element: <UserProfileContent /> },
+            { path: "contact-info", element: <ContactInfoPage /> },
+            { path: "settings", element: <SettingsPage /> },
+            { path: "payments", element: <PaymentsPage /> },
+            { path: "support", element: <SupportPage /> },
+        ]
     },
     {
-        path: "/eventPage",
-        element: <EventPage />
+        path: "/photoCarousel",
+        element: <PhotoCarousel />
     },
     {
         path: "*",
@@ -42,8 +59,8 @@ const router = createBrowserRouter([
     },
 ]);
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
 );
