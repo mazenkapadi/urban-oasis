73b3834a98e4ae4d7522a5831b21abba4e4fafd9
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-20 23:50:18 -0400

    major changes / updates

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import HeaderComponent from "./HeaderComponent.jsx";
+import SearchBarComponent from "./SearchBarComponent.jsx";
+
+const Header = () => {
+    return (
+        <>
+            <HeaderComponent />
+            <SearchBarComponent />
+        </>
+    )
+}
+
+export default Header;
