12e4ee00cface5f7631bbc42609a872bc029ae1a
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-21 12:00:58 -0400

    More mild changes

--- src/components/PhotoCarousel.jsx
+++ src/components/PhotoCarousel.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 
 
@@ -35,34 +35,34 @@ const PhotoCarousel = () => {
             }
         ],
         nextArrow: (
-            <div>
-                <div className="next-slick-arrow">
+            <div >
+                <div className="next-slick-arrow" >
                     <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="24" viewBox="0 -960 960 960"
-                         width="24">
+                         width="24" >
                         <path
-                            d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/>
-                    </svg>
-                </div>
-            </div>
+                            d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z" />
+                    </svg >
+                </div >
+            </div >
         ),
 
         prevArrow: (
-            <div>
-                <div className="next-slick-arrow rotate-180">
+            <div >
+                <div className="next-slick-arrow rotate-180" >
                     <svg xmlns="http://www.w3.org/2000/svg" stroke="black" height="24" viewBox="0 -960 960 960"
-                         width="24">
+                         width="24" >
                         <path
-                            d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z"/>
-                    </svg>
-                </div>
-            </div>
+                            d="m242-200 200-280-200-280h98l200 280-200 280h-98Zm238 0 200-280-200-280h98l200 280-200 280h-98Z" />
+                    </svg >
+                </div >
+            </div >
         ),
         afterChange: (currentSlide) => {
             setActiveSlide(currentSlide);
         },
     };
 
-    const [activeSlide, setActiveSlide] = useState(0);
+    const [ activeSlide, setActiveSlide ] = useState(0);
     const images = [
         'src/TestImages/TestImage1.jpg',
         'src/TestImages/TestImage2.jpg',
@@ -70,16 +70,17 @@ const PhotoCarousel = () => {
     ];
 
     return (
-        <div className="items-center justify-center p-8 ">
+        <div className="items-center justify-center p-8 " >
             <Slider {...carouselSettings}>
                 {images.map((image, index) => (
                     <div key={index}
-                         className={`relative ${index === activeSlide ? 'opacity-100' : 'opacity-50 hover:opacity-75 transition duration-300 ease-in-out'} flex px-4`}>
-                        <img src={image} alt={`Imagerwrwerwerwerwe ${index + 1}`} className="w-[400px] h-[300px] object-cover"/>
-                    </div>
+                         className={`relative ${index === activeSlide ? 'opacity-100' : 'opacity-50 hover:opacity-75 transition duration-300 ease-in-out'} flex px-4`} >
+                        <img src={image} alt={`Image ${index + 1}`}
+                             className="w-[400px] h-[300px] object-cover rounded-lg" />
+                    </div >
                 ))}
-            </Slider>
-        </div>
+            </Slider >
+        </div >
     )
         ;
 };
