9fdefa298eee7776c40d34d24db48f033f2bf1f1
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-23 11:09:27 -0400

    STRUCTURE

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,31 +1,15 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import FIREBASE_KEY from "./APIKEY_SECRETS/FIREBASE_KEY.js"
-import {
-    GoogleAuthProvider,
-    signInWithPopup,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signInWithRedirect,
-    getRedirectResult,
-} from 'firebase/auth';
+
 
 const app = initializeApp(FIREBASE_KEY);
 
 const auth = getAuth(app);
+const db = getFirestore(app);
 const googleProvider = new GoogleAuthProvider();
 
-export {
-    auth,
-    googleProvider,
-    signInWithPopup,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signInWithRedirect,
-    getRedirectResult
-};
 
-const db = getFirestore(app);
 
-export { db };
+export { db, auth, googleProvider };
