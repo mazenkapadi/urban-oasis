381147010eabad761f5c022d7371b9a1bdd53d70
dmondal06 <deepa.mondal321@gmail.com>
2024-09-24 19:27:46 -0400

    fixed sidebar

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -4,11 +4,11 @@ import SideBar from "../components/SideBar";
 
 const UserProfilePage = () => {
     return (
-        <div className="bg-gray-100 min-h-screen flex justify-start p-0">
-            <div className="w-1/6 bg-gray-900 sticky top-0 h-screen rounded-lg">
+        <div className="bg-gray-100 min-h-screen flex p-0">
+            <div className="w-1/6 bg-gray-900 fixed top-0 left-0 h-screen rounded-lg">
                 <SideBar />
             </div>
-            <div className="flex-grow p-8">
+            <div className="flex-grow p-8 ml-[16.6667%]">
                 <div className="bg-gray-900 text-white shadow-md rounded-lg p-6 h-full min-h-screen">
                     <Outlet />
                 </div>
