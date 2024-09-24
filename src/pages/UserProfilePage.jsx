6494bca251d7fd20117b716660f5503fba50eda1
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-09-24 11:12:49 -0400

    Pray tg

--- src/pages/UserProfilePage.jsx
+++ src/pages/UserProfilePage.jsx
@@ -5,14 +5,10 @@ import { Outlet } from 'react-router-dom';
 const UserProfilePage = () => {
     return (
         <div className="bg-gray-100 min-h-screen flex justify-start p-0">
-            {/* Sidebar (fixed width and dynamic height based on content) */}
             <div className="w-1/6 bg-gray-900 sticky top-0 h-screen rounded-lg">
                 <SideBar />
             </div>
-
-            {/* Content area (takes the remaining width) */}
             <div className="flex-grow p-8">
-                {/* Content wrapper */}
                 <div className="bg-gray-900 text-white shadow-md rounded-lg p-6 h-full min-h-screen">
                     <Outlet />
                 </div>
