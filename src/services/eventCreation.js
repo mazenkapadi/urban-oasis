81669bb5988908560a0346246d19332aae45fd9a
rosstopher14 <144082134+rosstopher14@users.noreply.github.com>
2024-09-24 08:46:48 -0400

    Event Creation  9-23

--- src/services/eventCreation.js
+++ src/services/eventCreation.js
@@ -1,10 +1,16 @@
 import {collection, doc, setDoc} from "firebase/firestore";
-import {db} from "../firebaseConfig.js";
+import {db, auth} from "../firebaseConfig.js";
 
 class EventCreation {
     async writeEventData(eventData) {
         console.log('Attempting to write data...');
         try {
+            const user = auth.currentUser;
+            if (!user) {
+                console.error("User not authenticated.");
+                return;
+            }
+
             const eventRef = doc(collection(db, "Events"));
             await setDoc(eventRef, eventData);
             console.log('Event created successfully! ID: ', eventRef.id);
