6a9bc118a90953b8281d0df1779057e5595176c4
mafareed <azmat.fareed123@gmail.com>
2024-09-27 14:15:16 +0500

    add: forecast + current location components

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -134,8 +134,6 @@
 //
 // export default HeaderComponent;
 
-
-
 import { useState, useEffect } from "react";
 import LogoComponent from "./LogoComponent";
 import ProfileOrSignIn from "./ProfileOrSignIn";
@@ -145,39 +143,39 @@ import SearchBarComponent from "./SearchBarComponent";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 const HeaderComponent = () => {
-    const [menuOpen, setMenuOpen] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-    useEffect(() => {
-        const auth = getAuth();
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            setIsLoggedIn(!!user);
-        });
-        return () => unsubscribe();
-    }, []);
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsLoggedIn(!!user);
+    });
+    return () => unsubscribe();
+  }, []);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-    return (
-        <>
-            <div className="fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-primary-dark p-2" >
-                <div className="p-2" >
-                    <div className="flex flex-col md:flex-row justify-between items-center" >
-                        <LogoComponent />
-                        <SearchBarComponent />
-                        <div className="flex items-center space-x-4" >
-                            <ProfileOrSignIn isLoggedIn={isLoggedIn} />
-                            <MenuButton toggleMenu={toggleMenu} />
-                        </div >
-                    </div >
-                </div >
-                <DropdownMenu isOpen={menuOpen} />
-            </div >
-        </>
-    );
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full z-50 transition-all duration-300 bg-primary-dark p-2">
+        <div className="p-2">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <LogoComponent />
+            <SearchBarComponent />
+            <div className="flex items-center space-x-4">
+              <ProfileOrSignIn isLoggedIn={isLoggedIn} />
+              <MenuButton toggleMenu={toggleMenu} />
+            </div>
+          </div>
+        </div>
+        <DropdownMenu isOpen={menuOpen} />
+      </div>
+    </>
+  );
 };
 
 export default HeaderComponent;
