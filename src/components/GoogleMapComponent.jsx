0abde473fee0b39735205f0a0301b0c51acffb98
mafareed <azmat.fareed123@gmail.com>
2024-09-28 15:59:15 +0500

    add: google map component and redirect link

---
+++ src/components/GoogleMapComponent.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+
+// Define the container styles for the map
+const containerStyle = {
+  width: "50%",
+  height: "500px",
+};
+
+const GoogleMaps = ({ latitude, longitude }) => {
+  const [mapLoaded, setMapLoaded] = useState(false); // State to track if the map has fully loaded
+
+  // Load the Google Maps script with the API key
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: import.meta.env.VITE_GM_KEY,
+  });
+
+  // Check if there's an error or the script is not loaded
+  if (loadError) return <div>Error loading maps</div>;
+  if (!isLoaded) return <div>Loading Maps...</div>;
+
+  // Set the center of the map using the provided latitude and longitude
+  const center = {
+    lat: latitude || 0,
+    lng: longitude || 0,
+  };
+
+  // Map load event handler
+  const handleMapLoad = () => {
+    setMapLoaded(true); // Set state when the map has loaded
+  };
+  const handleMapClick = () => {
+    // Construct the Google Maps URL with a marker
+    const googleMapsUrl = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}&markers=${latitude},${longitude}`;
+    window.open(googleMapsUrl, "_blank"); // Open the URL in a new tab
+  };
+
+  return (
+    <div className="w-full h-[500px]">
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={12}
+        onLoad={handleMapLoad}
+        onClick={handleMapClick} // Trigger when map loads
+      >
+        {/* Only show the marker when the map is fully loaded */}
+        {mapLoaded && <MarkerF position={center} />}
+      </GoogleMap>
+    </div>
+  );
+};
+
+export default GoogleMaps;
