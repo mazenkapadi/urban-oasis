4e4b80cef27272f09740ea0abb3d30c2c14878fd
rosstopher14 <144082134+rosstopher14@users.noreply.github.com>
2024-09-28 00:00:39 -0400

    Event 9-28

--- src/components/EventCarousel.jsx
+++ src/components/EventCarousel.jsx
@@ -11,8 +11,8 @@ const EventCarousel = () => {
     const [events, setEvents] = useState([]);
     const navigate = useNavigate();
 
-    const handleCardClick = (eventId, event) => {
-        navigate(`/eventPage/${eventId}`, {state: { event }});
+    const handleCardClick = (eventId) => {
+        navigate(`/eventPage/${eventId}`);
     };
 
     useEffect(() => {
@@ -74,14 +74,13 @@ const EventCarousel = () => {
                         <Link key={event.id} to={`/eventPage/${event.id}`}>
                             <EventCard
                                 key={event.id}
-                                onClick={() => handleCardClick(event.id)}
+                                onClick={() => handleCardClick(event.id, event)}
                                 title={event.basicInfo.title}
                                 location={event.basicInfo.location}
                                 date={event.eventDetails.eventDateTime.toDate().toLocaleDateString()}
                                 price={event.eventDetails.eventPrice}
                                 image={event.eventDetails.images[0] || 'defaultImageURL'}
                                 eventId={event.id}
-                                event={event}
                             />
                         </Link>
                     ))}
