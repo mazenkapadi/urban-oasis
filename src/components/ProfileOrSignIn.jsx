475fd9b33b1230f13535b95cfd6011c9e733f182
dmondal06 <deepa.mondal321@gmail.com>
2024-10-07 22:21:21 -0400

    suppress

--- src/components/ProfileOrSignIn.jsx
+++ src/components/ProfileOrSignIn.jsx
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from "../firebaseConfig.js";
 
+// eslint-disable-next-line react/prop-types
 const ProfileOrSignIn = ({ isLoggedIn }) => {
     const navigate = useNavigate();
+    const [profilePic, setProfilePic] = useState('');
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                try {
+                    const userDocRef = doc(db, "Users", user.uid);
+                    const userDocSnap = await getDoc(userDocRef);
+
+                    if (userDocSnap.exists()) {
+                        const userData = userDocSnap.data();
+
+                        // Set the profile picture from Firestore
+                        const profilePicURL = userData.profilePic || "";
+                        setProfilePic(profilePicURL);
+                    } else {
+                        console.log("No such document in Firestore!");
+                    }
+                } catch (error) {
+                    console.error("Error fetching profile data from Firestore:", error);
+                }
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
 
     const handleSignIn = () => {
         navigate("/signIn");
@@ -10,15 +41,21 @@ const ProfileOrSignIn = ({ isLoggedIn }) => {
     return (
         <div className="flex items-center space-x-4">
             {isLoggedIn ? (
+                profilePic ? (
                     <button
-                    onClick={() => navigate("/userProfilePage")}
-                        className="w-10 h-10 rounded-lg overflow-hidden">
+                        onClick={() => navigate("/userProfilePage")}
+                        className="w-10 h-10 rounded-lg overflow-hidden"
+                    >
                         <img
-                            src="https://picsum.photos/32"
+                            src={profilePic}
                             alt="User Profile"
                             className="w-full h-full object-cover"
                         />
                     </button>
+                ) : (
+                    <div className="w-10 h-10 rounded-lg overflow-hidden bg-gray-500 flex items-center justify-center">
+                    </div>
+                )
             ) : (
                 <button
                     className="rounded-lg bg-gray-900 text-gray-200 px-6 py-2"
