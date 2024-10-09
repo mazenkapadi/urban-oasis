a332b3d811732e113649461bbb50734f07d299b3
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-10-08 23:41:14 -0400

    blah blah

--- src/components/WideEventCard.jsx
+++ src/components/WideEventCard.jsx
@@ -12,7 +12,7 @@ const WideEventCard = ({ event }) => {
     return (
         <div
             key={event.id}
-            className="flex p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer bg-white"
+            className="flex p-4 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 cursor-pointer bg-neutral-white"
             onClick={handleNavigate}
         >
             <img
@@ -22,36 +22,33 @@ const WideEventCard = ({ event }) => {
             />
             <div className="ml-4 flex flex-col justify-between flex-grow">
                 <div className="flex justify-between items-center">
-                    <h2 className="text-xl font-semibold text-[#2B2D42]">{event.basicInfo.title}</h2>
-                    <p className="ml-4 text-lg font-bold text-[#2B2D42]">
+                    <h2 className="text-xl font-semibold text-black">{event.basicInfo.title}</h2>
+                    <p className="ml-4 text-lg font-bold text-black">
                         {event.eventDetails.paidEvent ? (
-                            <div className="flex flex-row items-center">
+                            <div className="flex items-center">
                                 <TagIcon className="inline-block w-5 h-5 mr-1" />
                                 ${event.eventDetails.eventPrice.toFixed(2)}
                             </div>
                         ) : (
-                            <span>Free Entry</span>
+                            <span>Free RSVP</span>
                         )}
                     </p>
                 </div>
-                <div className="flex items-center text-[#8D99AE]">
-                    <MapPinIcon className="w-5 h-5 mr-1" />
+                <div className="flex items-center text-detail-gray">
+                    <MapPinIcon className="w-5 h-5" />
                     <p className="text-sm">{event.basicInfo.location.label}</p>
                 </div>
-                <div className="flex items-center text-[#8D99AE]">
-                    <CalendarIcon className="w-5 h-5 mr-1" />
+                <div className="flex items-center text-detail-gray">
+                    <CalendarIcon className="w-5 h-5" />
                     <p className="text-sm">
-                        {`${event.eventDetails.eventDateTime
-                                 .toDate()
-                                 .toLocaleString("en-US", {
-                                     year: "numeric",
-                                     month: "long",
-                                     day: "numeric",
-                                     hour: "numeric",
-                                     minute: "numeric",
-                                     hour12: true,
-                                 })
-                                 .replace(",", " at")}`}
+                        {`${event.eventDetails.eventDateTime.toDate().toLocaleString("en-US", {
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                            hour: "numeric",
+                            minute: "numeric",
+                            hour12: true,
+                        })}`}
                     </p>
                 </div>
             </div>
