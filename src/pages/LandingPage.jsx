39598226ffc6f9ad91484d86f34e21c118be09fc
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-20 22:30:17 -0500

    BookMarks bug fixed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -41,7 +41,6 @@ function LandingPage() {
 
 
                         <div >
-                            <span className="text-3xl font-bold" >User Testimonials</span >
                             <Testimonials />
                         </div >
                     </div >
