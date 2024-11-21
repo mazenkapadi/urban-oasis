7e6a077178ec95f88275d0d1cfce240d48c27b55
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-20 22:31:36 -0500

    BookMarks bug fixed

--- src/algoliaConfig.js
+++ src/algoliaConfig.js
@@ -8,9 +8,9 @@ import { algoliaAppId, algoliaSearchKey } from './APIKEY_SECRETS/ALGOLIA_KEY';
 
 const searchClient = algoliasearch(algoliaAppId, algoliaSearchKey);
 
-console.log('Algolia App ID:', algoliaAppId);
-console.log('Algolia Search Key:', algoliaSearchKey);
-console.log('Search Client:', searchClient);
-console.log('Available Methods on Search Client:', Object.keys(searchClient));
+// console.log('Algolia App ID:', algoliaAppId);
+// console.log('Algolia Search Key:', algoliaSearchKey);
+// console.log('Search Client:', searchClient);
+// console.log('Available Methods on Search Client:', Object.keys(searchClient));
 
 export { searchClient };
