dd001e291618ca268568fba56b5a1bb6fb30fc21
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-23 18:18:22 -0500

    categoryData.js

---
+++ src/services/categoryData.js
@@ -0,0 +1,26 @@
+export const categorizedOptions = {
+    'Arts & Entertainment': [
+        'Music', 'Art', 'Comedy', 'Theater & Performing Arts',
+        'Film & Media', 'Photography & Art Exhibits', 'Opera',
+    ],
+    'Business & Networking': [
+        'Business', 'Networking', 'Politics & Activism',
+        'Charity & Fundraisers', 'Conferences',
+    ],
+    'Education & Innovation': [
+        'Technology', 'Science & Innovation', 'Education',
+        'Workshops & Classes', 'Talks & Seminars', 'Online Courses'
+    ],
+    'Lifestyle & Wellness': [
+        'Health', 'Spirituality & Wellness', 'Family & Kids',
+        'Fashion & Beauty', 'Mental Health',
+    ],
+    'Food & Leisure': [
+        'Food & Drink', 'Cooking & Culinary', 'Shopping & Markets',
+        'Travel & Outdoor', 'Wine Tasting', 'Dining Experiences'
+    ],
+    'Sports & Recreation': [
+        'Sports', 'Gaming & E-sports', 'Fitness & Training',
+        'Adventure Sports', 'Hiking & Nature',
+    ],
+};
