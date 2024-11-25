1a9050817a6ee159a99a0168ad542b5d283c64f6
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-24 22:25:46 -0500

    Fixes

--- src/pages/Host/HostEventPage.jsx
+++ src/pages/Host/HostEventPage.jsx
@@ -9,29 +9,29 @@ import FooterComponent from "../../components/FooterComponent.jsx";
 import LoadingPage from "../service/LoadingPage.jsx";
 
 const HostEventPage = () => {
-    const { eventId } = useParams();
-    const [eventTitle, setEventTitle] = useState('');
-    const [eventDescription, setEventDescription] = useState('');
-    const [eventLocation, setEventLocation] = useState('');
-    const [eventDateTime, setEventDateTime] = useState('');
-    const [eventPrice, setEventPrice] = useState('');
-    const [eventImages, setEventImages] = useState([]);
-    const [isPaidEvent, setIsPaidEvent] = useState(false);
-    const [userId, setUserId] = useState(null);
+    const {eventId} = useParams();
+    const [ eventTitle, setEventTitle ] = useState('');
+    const [ eventDescription, setEventDescription ] = useState('');
+    const [ eventLocation, setEventLocation ] = useState('');
+    const [ eventDateTime, setEventDateTime ] = useState('');
+    const [ eventPrice, setEventPrice ] = useState('');
+    const [ eventImages, setEventImages ] = useState([]);
+    const [ isPaidEvent, setIsPaidEvent ] = useState(false);
+    const [ userId, setUserId ] = useState(null);
 
-    const [hostDetails, setHostDetails] = useState({
+    const [ hostDetails, setHostDetails ] = useState({
         bio: '',
         companyName: '',
         website: '',
-        hostLocation: { city: '', state: '' },
+        hostLocation: {city: '', state: ''},
     });
 
-    const [loading, setLoading] = useState(true);
-    const [isHost, setIsHost] = useState(false); // To check if user is host
-    const [editable, setEditable] = useState(false); // Edit mode for host details
-    const [ticketQuantity, setTicketQuantity] = useState([]);
-    const [attendeeId, setAttendeeId] = useState([]);
-    const [attendeeDetails, setAttendeeDetails] = useState([]);
+    const [ loading, setLoading ] = useState(true);
+    const [ isHost, setIsHost ] = useState(false); // To check if user is host
+    const [ editable, setEditable ] = useState(false); // Edit mode for host details
+    const [ ticketQuantity, setTicketQuantity ] = useState([]);
+    const [ attendeeId, setAttendeeId ] = useState([]);
+    const [ attendeeDetails, setAttendeeDetails ] = useState([]);
 
     // Get Authenticated User ID
     useEffect(() => {
@@ -73,7 +73,7 @@ const HostEventPage = () => {
                                 bio: hostData.bio || '',
                                 companyName: hostData.companyName || '',
                                 website: hostData.website || '',
-                                hostLocation: hostData.hostLocation || { city: '', state: '' }
+                                hostLocation: hostData.hostLocation || {city: '', state: ''}
                             });
                         }
                     }
@@ -87,7 +87,7 @@ const HostEventPage = () => {
                     const rsvps = data.rsvps;
                     const attendees = [];
 
-                    for (const [key, innerMap] of Object.entries(rsvps)) {
+                    for (const [ key, innerMap ] of Object.entries(rsvps)) {
                         const userId = innerMap.userId;
                         const quantity = innerMap.quantity;
 
@@ -100,6 +100,7 @@ const HostEventPage = () => {
                                 firstName: attendeeData.name.firstName,
                                 lastName: attendeeData.name.lastName,
                                 quantity,
+                                email: attendeeData.contact.email,
                             });
                         }
                     }
@@ -112,7 +113,7 @@ const HostEventPage = () => {
         };
 
         fetchEventData();
-    }, [eventId, userId]);
+    }, [ eventId, userId ]);
 
     const toggleEditMode = () => setEditable(!editable);
 
@@ -135,8 +136,8 @@ const HostEventPage = () => {
     };
 
     const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setHostDetails((prevDetails) => ({ ...prevDetails, [name]: value }));
+        const {name, value} = e.target;
+        setHostDetails((prevDetails) => ({...prevDetails, [name]: value}));
     };
 
     if (loading) {
@@ -153,21 +154,21 @@ const HostEventPage = () => {
     return (
         <>
             <HeaderComponent />
-            <div className="event-page py-10 px-4 pt-32 bg-gray-200 min-h-screen">
-                <div className="box-border bg-white p-8 rounded-lg shadow-lg">
+            <div className="event-page py-10 px-4 pt-32 bg-gray-200 min-h-screen" >
+                <div className="box-border bg-white p-8 rounded-lg shadow-lg" >
                     <PhotoCarousel eventId={eventId} eventTitle={eventTitle} />
 
-                    <div className="flex flex-col mt-6">
-                        <h1 className="text-3xl font-bold">{eventTitle}</h1>
-                        <p className="text-gray-500">{eventDateTime}</p>
-                        <p className="mt-4">{eventDescription}</p>
+                    <div className="flex flex-col mt-6" >
+                        <h1 className="text-3xl font-bold" >{eventTitle}</h1 >
+                        <p className="text-gray-500" >{eventDateTime}</p >
+                        <p className="mt-4" >{eventDescription}</p >
 
                         {isHost && (
-                            <div className="host-details mt-8">
-                                <h2 className="text-2xl font-bold">Host Information</h2>
+                            <div className="host-details mt-8" >
+                                <h2 className="text-2xl font-bold" >Host Information</h2 >
 
-                                <div className="mt-4">
-                                    <label className="block text-gray-600">Company Name</label>
+                                <div className="mt-4" >
+                                    <label className="block text-gray-600" >Company Name</label >
                                     <input
                                         type="text"
                                         name="companyName"
@@ -177,7 +178,7 @@ const HostEventPage = () => {
                                         className="input-field"
                                     />
 
-                                    <label className="block text-gray-600 mt-4">Bio</label>
+                                    <label className="block text-gray-600 mt-4" >Bio</label >
                                     <textarea
                                         name="bio"
                                         value={hostDetails.bio}
@@ -186,7 +187,7 @@ const HostEventPage = () => {
                                         className="input-field"
                                     />
 
-                                    <label className="block text-gray-600 mt-4">Website</label>
+                                    <label className="block text-gray-600 mt-4" >Website</label >
                                     <input
                                         type="text"
                                         name="website"
@@ -196,7 +197,7 @@ const HostEventPage = () => {
                                         className="input-field"
                                     />
 
-                                    <label className="block text-gray-600 mt-4">Location</label>
+                                    <label className="block text-gray-600 mt-4" >Location</label >
                                     <input
                                         type="text"
                                         name="hostLocation.city"
@@ -216,50 +217,90 @@ const HostEventPage = () => {
                                         placeholder="State"
                                     />
 
-                                    <div className="mt-4">
+                                    <div className="mt-4" >
                                         <button
                                             className={`button ${editable ? 'bg-green-500' : 'bg-blue-500'}`}
                                             onClick={toggleEditMode}
                                         >
                                             {editable ? 'Save' : 'Edit'}
-                                        </button>
+                                        </button >
                                         {editable && (
                                             <button
                                                 className="button bg-gray-500 ml-2"
                                                 onClick={handleUpdateHostDetails}
                                             >
                                                 Save Changes
-                                            </button>
+                                            </button >
                                         )}
-                                    </div>
-                                </div>
-                            </div>
+                                    </div >
+                                </div >
+                            </div >
                         )}
-                    </div>
+                    </div >
                     {isHost && (
-                        <div>
-                            <label className="block text-gray-600 mt-4">Attendees</label>
-                            <div className="">
-                                <div
-                                    className="block text-gray-600 mt-4">{attendeeDetails.map((attendee, index) => (
-                                    <div className="flex items-center" key={index}>
-                                        <p>{attendee.firstName} {attendee.lastName} -
-                                            Held Tickets: {attendee.quantity}</p>
-                                        <button className="button bg-gray-500 mr-2 ml-auto"
-                                                onClick={handleModifyButton}>
-                                            Modify Quantity
-                                        </button>
-                                        <button className="button bg-gray-500"
-                                                onClick={handleContactButton}>
-                                            Contact Attendee
-                                        </button>
-                                    </div>
-                                ))}</div>
-                            </div>
-                        </div>
+                        // <div>
+                        //     <label className="block text-gray-600 mt-4">Attendees</label>
+                        //     <div className="">
+                        //         <div
+                        //             className="block text-gray-600 mt-4">{attendeeDetails.map((attendee, index) => (
+                        //             <div className="flex items-center" key={index}>
+                        //                 <p>{attendee.firstName} {attendee.lastName} -
+                        //                     Tickets held: {attendee.quantity}</p>
+                        //                 <button className="button bg-accent-purple m-2 ml-auto"
+                        //                         onClick={handleModifyButton}>
+                        //                     Modify Quantity
+                        //                 </button>
+                        //                 <button className="button bg-gray-500"
+                        //                         onClick={handleContactButton}>
+                        //                     Contact Attendee
+                        //                 </button>
+                        //             </div>
+                        //         ))}</div>
+                        //     </div>
+                        // </div>
+
+                        <div className="mt-6" >
+                            <label className=" text-Dark-D2a font-lalezar text-body font-medium mb-2" >
+                                Attendees
+                            </label >
+                            <div className="bg-primary-light rounded-lg p-4 shadow-md" >
+                                {attendeeDetails.map((attendee, index) => (
+                                    <div
+                                        className="flex items-center justify-between border-b border-Light-L3 py-3"
+                                        key={index}
+                                    >
+                                        <div className="flex flex-col" >
+                                            <p className="text-primary-dark font-archivo text-body font-bold" >
+                                                {attendee.firstName} {attendee.lastName}
+                                                <br />
+                                                {attendee.email}
+                                            </p >
+                                            <p className="text-accent-orange font-archivo text-h4 font-bold" >
+                                                Tickets: {attendee.quantity}
+                                            </p >
+                                        </div >
+
+                                        <div className="flex items-center gap-2" >
+                                            <button
+                                                className="bg-accent-purple text-neutral-white font-roboto text-button font-bold px-4 py-2 rounded-lg transition-transform transform hover:scale-105"
+                                                onClick={handleModifyButton}
+                                            >
+                                                Cancel Ticket
+                                            </button >
+                                            <button
+                                                className="bg-Dark-D1 text-Light-L1 font-roboto text-button font-bold px-4 py-2 rounded-lg transition-transform transform hover:scale-105"
+                                                onClick={handleContactButton}
+                                            >
+                                                Contact Attendee
+                                            </button >
+                                        </div >
+                                    </div >
+                                ))}
+                            </div >
+                        </div >
                     )}
-                </div>
-            </div>
+                </div >
+            </div >
             <FooterComponent />
         </>
     );
