391170df4b8703ae0f8e6cc3257b4a0a4da8e34e
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-25 16:10:29 -0500

    Stripe Webhook

--- api/paymentwebhook.js
+++ api/paymentwebhook.js
@@ -1,18 +1,35 @@
-import { db } from '../src/firebaseConfig.js';
-import { doc, setDoc, updateDoc, increment } from 'firebase/firestore';
+import { doc, setDoc, updateDoc, increment, getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+
+const FIREBASE_KEY = {
+    apiKey: process.env.VITE_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    weatherApiKey: process.env.VITE_WEATHER_API_KEY,
+    geoApiKey: process.env.VITE_WEATHER_API_KEY,
+    gmKey: process.env.VITE_WEATHER_GM_KEY,
+    locationKey: process.env.VITE_LOCATION_KEY,
+}
+const app = initializeApp(FIREBASE_KEY);
+
+const db = getFirestore(app);
 
 export async function POST(req) {
     try {
         const event = await req.json();
 
         if (event.type !== 'checkout.session.completed') {
-            return new Response('Event type not handled', { status: 400 });
+            return new Response('Event type not handled', {status: 400});
         }
 
         const session = event.data.object;
 
-        const { metadata, amount_total, customer_details } = session;
-        const { userId, eventId, quantity } = metadata;
+        const {metadata, amount_total, customer_details} = session;
+        const {userId, eventId, quantity} = metadata;
         const email = customer_details?.email;
 
         const rsvpData = {
@@ -35,7 +52,7 @@ export async function POST(req) {
                     [session.id]: rsvpData,
                 },
             },
-            { merge: true }
+            {merge: true}
         );
 
         await setDoc(
@@ -45,7 +62,7 @@ export async function POST(req) {
                     [session.id]: rsvpData,
                 },
             },
-            { merge: true }
+            {merge: true}
         );
 
         await updateDoc(eventDocRef, {
@@ -54,9 +71,9 @@ export async function POST(req) {
 
         console.log('Checkout session processed successfully:', session.id);
 
-        return new Response('Event processed successfully', { status: 200 });
+        return new Response('Event processed successfully', {status: 200});
     } catch (error) {
         console.error('Error processing webhook:', error);
-        return new Response('Error processing webhook', { status: 500 });
+        return new Response('Error processing webhook', {status: 500});
     }
 }
