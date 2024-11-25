f46176da57974ae2002a7615b3d4dd5dc25657e5
rosstopher14 <144082134+rosstopher14@users.noreply.github.com>
2024-11-24 19:28:55 -0500

    Carousel fix, event location fix, event date/time kinda fix

--- src/pages/EventCreationPage.jsx
+++ src/pages/EventCreationPage.jsx
@@ -311,6 +311,10 @@ function EventCreationPage() {
                                         selectProps={{
                                             value: eventLocation, onChange: setEventLocation,
                                             styles: {
+                                                placeholder: (base) => ({
+                                                    ...base,
+                                                    color: '#9CA3AF'
+                                                }),
                                                 control: (provided, state) => ({
                                                     ...provided,
                                                     backgroundColor: '#1F2937',
@@ -325,16 +329,19 @@ function EventCreationPage() {
                                                     ...provided,
                                                     backgroundColor: '#1F2937',
                                                     borderColor: '#374151',
+                                                    maxHeight: "200px",
+                                                    overflowY: "auto",
                                                 }),
                                                 input: (provided) => ({
                                                     ...provided,
                                                     color: "white",
                                                     border: 'none',
                                                 }),
-                                                option: (provided) => ({
+                                                option: (provided, state) => ({
                                                     ...provided,
-                                                    color: "white",
-                                                    backgroundColor: '#1F2937',
+                                                    color: state.isFocused ? "#6366F1" : "white",
+                                                    backgroundColor: state.isFocused ? "#374151" : "#1F2937",
+                                                    cursor: "pointer",
                                                 }),
                                                 singleValue: (provided) => ({
                                                     ...provided,
@@ -360,14 +367,24 @@ function EventCreationPage() {
                                         <label htmlFor="eventDateTime" className="text-lg font-semibold text-white">Event
                                             Date
                                             and Time</label>
-                                        <input
-                                            type="datetime-local"
-                                            id="eventDateTime"
-                                            value={eventDateTime}
-                                            onChange={(e) => setEventDateTime(e.target.value)}
-                                            className="w-full mt-2 p-3 rounded-md border border-gray-700 bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
-                                            required
-                                        />
+                                        <div className="relative mt-2">
+                                            <input
+                                                type="datetime-local"
+                                                id="eventDateTime"
+                                                value={eventDateTime}
+                                                onChange={(e) => setEventDateTime(e.target.value)}
+                                                className="w-full p-3 rounded-md border border-gray-700 bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                                                required
+                                            />
+                                            <div
+                                                className="absolute inset-y-0 right-3 flex items-center text-gray-400 pointer-events-none">
+                                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"
+                                                     viewBox="0 0 24 24" stroke="currentColor">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                                                          d="M8 7V3m8 4V3m-9 4h10M3 21h18a2 2 0 002-2V7a2 2 0 00-2-2H3a2 2 0 00-2 2v12a2 2 0 002 2z"/>
+                                                </svg>
+                                            </div>
+                                        </div>
                                     </div>
                                     <div className="flex-1">
                                         <label htmlFor="eventCapacity"
