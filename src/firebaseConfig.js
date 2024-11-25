e5f8d9f162ea0f71e90653518e9c7ec926cb169c
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-25 16:01:31 -0500

    Stripe Webhook

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,17 +4,17 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const FIREBASE_KEY = {
-    apiKey: process.env.VITE_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.VITE_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.VITE_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    weatherApiKey: process.env.VITE_WEATHER_API_KEY,
-    geoApiKey: process.env.VITE_WEATHER_API_KEY,
-    gmKey: process.env.VITE_WEATHER_GM_KEY,
-    locationKey: process.env.VITE_LOCATION_KEY,
+    apiKey: import.meta.env.VITE_PUBLIC_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_PUBLIC_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_PUBLIC_FIREBASE_MEASUREMENT_ID,
+    weatherApiKey: import.meta.env.VITE_WEATHER_API_KEY,
+    geoApiKey: import.meta.env.VITE_WEATHER_API_KEY,
+    gmKey: import.meta.env.VITE_WEATHER_GM_KEY,
+    locationKey: import.meta.env.VITE_LOCATION_KEY,
 }
 const app = initializeApp(FIREBASE_KEY);
 
