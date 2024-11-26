8d66cd5e117230f53401f80e39ca5342f840e996
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-25 21:32:28 -0500

    Dropdown Menu

--- src/pages/EventCreationPage.jsx
+++ src/pages/EventCreationPage.jsx
@@ -370,6 +370,14 @@ function EventCreationPage() {
                                                 className="w-full p-3 rounded-md border border-gray-700 bg-gray-800 text-white focus:outline-none focus:ring-2 focus:ring-indigo-500"
                                                 required
                                             />
+                                            {/*<div*/}
+                                            {/*    className="absolute inset-y-0 right-3 flex items-center text-gray-400 pointer-events-none" >*/}
+                                            {/*    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none"*/}
+                                            {/*         viewBox="0 0 24 24" stroke="currentColor" >*/}
+                                            {/*        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}*/}
+                                            {/*              d="M8 7V3m8 4V3m-9 4h10M3 21h18a2 2 0 002-2V7a2 2 0 00-2-2H3a2 2 0 00-2 2v12a2 2 0 002 2z" />*/}
+                                            {/*    </svg >*/}
+                                            {/*</div >*/}
                                         </div >
                                     </div >
                                     <div className="flex-1" >
@@ -581,33 +589,6 @@ function EventCreationPage() {
                                         </div >
                                     ))}
                                 </div >
-
-                                {/*<div >*/}
-                                {/*    <label className="text-lg font-semibold text-white" >Categories</label >*/}
-                                {/*    <div className="space-y-4 mt-2" >*/}
-                                {/*        {Object.entries(categorizedOptions).map(([ categoryGroup, categories ], index) => (*/}
-                                {/*            <div key={index} className="border-b border-gray-700 pb-4" >*/}
-                                {/*                <h3 className="text-white font-bold" >{categoryGroup}</h3 >*/}
-                                {/*                <div className="grid grid-cols-2 gap-4 mt-2" >*/}
-                                {/*                    {categories.map((category) => (*/}
-                                {/*                        <label key={category}*/}
-                                {/*                               className="text-white flex items-center space-x-2" >*/}
-                                {/*                            <input*/}
-                                {/*                                type="checkbox"*/}
-                                {/*                                checked={[ selectedPrimaryCategory, ...selectedSubcategories ].includes(category)}*/}
-                                {/*                                onChange={() => handleSubcategoryChange(category)}*/}
-                                {/*                                className="form-checkbox h-4 w-4 text-indigo-500"*/}
-                                {/*                            />*/}
-                                {/*                            <span >{category}</span >*/}
-                                {/*                        </label >*/}
-                                {/*                    ))}*/}
-                                {/*                </div >*/}
-                                {/*            </div >*/}
-                                {/*        ))}*/}
-                                {/*    </div >*/}
-                                {/*</div >*/}
-
-
                                 <div >
                                     <label className="text-lg font-semibold text-white" >Primary Category</label >
                                     {/* Dropdown for Primary Category Selection */}
