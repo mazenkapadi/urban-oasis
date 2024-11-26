f2048d0e7173a9f11eaafef7f7edca391095f08f
mazenkapadi <73120392+mazenkapadi@users.noreply.github.com>
2024-11-25 21:36:53 -0500

    Dropdown Menu

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function FooterComponent() {
     return (
         <footer className="bg-primary-dark text-primary-light py-6">
-            <div className="container mx-auto px-6 md:px-12 flex flex-col md:flex-row justify-between items-center">
+            <div className="container mx-auto px-2 md:px-4 flex flex-col md:flex-row justify-between items-center">
                 <p className="text-sm">
                     © 2024 Urban Oasis. All rights reserved.
                 </p>
